9008ab837b0ed19c3a75f56726656406c837f1a8
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-11-05 15:28:41 +0800

    feat&refactor: hello world HttpServer + slightly unconventional file structure (to accomodate scala app in parallel)

--- rust-src/greet.rs
+++ rust-src/greet.rs
@@ -1,7 +1,8 @@
-fn main() {
+fn greet() {
     // NOTE: let is a STATEMENT that binds the expression "Hello" (&str) to msg
     // requires ; at the end
     let msg = "Hello, you... Wonderful world!";
+
     // NOTE:
     // the ! in println! indicates that is is a Macro, not a Function !!!
     // the ; at the end indicates a STATEMENT.
